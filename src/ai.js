7ff76fc3a763128f1860dfbd6418a263514b237c
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-04-01 14:25:49 -0400

    update comments

--- src/ai.js
+++ src/ai.js
@@ -7,9 +7,9 @@ const SYSTEM_PROMPT = `
  they could make with all of those ingredients. The recipe can include additional ingredients they 
  didn't mention, but try not to include too many extra ingredients. Format your response in 
  grammatically correct markdown to make it easier to render to a web page.
-`;
+`; //  defines the behavior of the model
 
-export async function getRecipeFromMistral(
+export async function getRecipeFromMistral( //  asynchronous function, which means it returns a Promise
   ingredientsArr,
   restrictionsArr,
   cookingTime,
@@ -22,17 +22,17 @@ export async function getRecipeFromMistral(
 
   try {
     const response = await hf.chatCompletion({
-      model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+      model: "mistralai/Mixtral-8x7B-Instruct-v0.1", // sending a request to this specific Hugging Face API model
       messages: [
-        { role: "system", content: SYSTEM_PROMPT },
+        { role: "system", content: SYSTEM_PROMPT }, // sets the context
         {
-          role: "user",
+          role: "user", // dynamically incorporates the formatted ingredient list, restrictions, cooking time, serving size
           content: `I have ${ingredientsString}. Please recommend a recipe that can be cooked 
           in ${cookingTimeNumber} minutes. Please do not include ${restrictionsString} in this recipe. 
           Please make sure the recipe has a serving size of ${servingSizeNumber}.`,
         },
       ],
-      max_tokens: 1024,
+      max_tokens: 1024, // response should not exceed 1024 tokens but I updated my Hugging Face plan to increase the tokens
     });
     return response.choices[0].message.content;
   } catch (err) {
